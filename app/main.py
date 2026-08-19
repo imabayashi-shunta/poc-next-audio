35011a589a49f75559e96894da514f1809a0fcfe
imabayashi-shunta <shunta_imabayashi@fnl.co.jp>
2026-08-19 15:05:19 +0900

    feat: GitHub Actionsの自動デプロイ設定(deploy.yml)を追加

--- app/main.py
+++ app/main.py
@@ -12,7 +12,7 @@ def main():
     xml_data = parse_xml("sample_data/PoC_test/STM2026072800333/EXN_STM2026072800333_20260728143556.XML")
     pprint(f"【XML抽出結果】\n{xml_data}")
     print("")
-    
+
     # CSVの検証
     csv_data = parse_csv("sample_data/PoC_test/EXI_天気：葛飾花火大会_20260728143554.CSV")
     pprint(f"【CSV抽出結果】\n{csv_data}")
@@ -21,6 +21,7 @@ def main():
     # 映像ファイルの検証
     mp4_data = parse_video("sample_data/PoC_test/20260818_次期音声PoCの映像素材.mp4")
     pprint(f"【MP4抽出結果】\n{mp4_data}")
+    print("")
 
 if __name__ == "__main__":
     main()
